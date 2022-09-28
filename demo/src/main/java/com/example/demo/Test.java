bd1ea48cc5faf72dabcd34fc60fc566e294b6034
DawnCc <321427545@qq.com>
2022-09-28 21:58:55 +0800

    test

---
+++ demo/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/**
+ * @author: gn
+ * @Desc:
+ * @create: 2022-09-28 21:57
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        System.out.println("test");
+    }
+}
